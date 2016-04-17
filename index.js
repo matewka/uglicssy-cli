125efe0af99712cbb60f6826c7737a42460f4110
Mateusz Gawliński <matewka@gmail.com>
2016-04-17 20:44:54 +0200

    Outputting to a file by default.

--- index.js
+++ index.js
@@ -9,24 +9,46 @@ const Uglicssy = require('uglicssy');
 const uglicssy = new Uglicssy();
 
 program
-  .usage('[options] <input_file...>')
-  .option('-o, --output', 'output file name')
+  .usage('[options] <input_file...> <output_file...>')
   .parse(process.argv);
-
-const inputFilename = program.args[program.args.length - 1];
 
 if (program.args.length === 0) {
   program.help();
-} else if (program.args.length <= program.options.length) {
+} else if (program.args < 1) {
   console.error('Input file name is required');
+} else if (program.args < 2) {
+  console.error('Output file name is required');
 } else {
+  const inputFilename = program.args[0];
+  const fileParts = inputFilename.split('.');
+  let outputFilename = program.args[1];
+  let fileType;
+
+  if (fileParts.length > 1) {
+    fileType = fileParts.slice(-1)[0];
+  }
+
+  if (!outputFilename) {
+    outputFilename = fileParts.slice(0, -1).join('.') + '.uglicssy';
+
+    if (fileType) {
+      outputFilename += '.' + fileType;
+    }
+  }
+
   fs.readFile(inputFilename, (err, data) => {
     if (!err) {
-      const output = uglicssy.convert(data.toString(), 'html');
+      const output = uglicssy.convert(data.toString(), fileType);
 
-      console.log(output);
+      fs.writeFile(outputFilename, output, (err) => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Uglicssy: ${inputFilename} -> ${outputFilename}`);
+      });
     } else {
-      console.error('An error occurred while reading the input file:', err);
+      console.error(`An error occurred while reading the input file: ${err}`);
     }
   });
 }
